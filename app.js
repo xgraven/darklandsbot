ed91ecb86adbf881d74733fa41fb904bae988492
xgraven <xgraven@gmail.com>
2018-09-23 13:10:07 +0300

    modules

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 var msg = "Hello, world!"
 console.log(msg);
+
+var TelegramBot = require('node-telegram-bot-api');
+
+// Устанавливаем токен, который выдавал нам бот.
+var token = '656336982:AAEN9CazxfCX091eNkLwXb2GMCR2dpG8j_g';
+// Включить опрос сервера
+var bot = new TelegramBot(token, {polling: true});
+
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
+bot.onText(/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id;
+    var resp = match[1];
+    bot.sendMessage(fromId, resp);
+});
+
+// Простая команда без параметров.
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+    // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
+    var photo = 'testimage.png';
+    bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
+});
